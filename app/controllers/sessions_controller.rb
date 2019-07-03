f8449a166d205568b9ecd459327ee73ff6b7a3d8
vutuananh <vutuananhsama@gmail.com>
2019-07-03 17:04:18 +0700

    chapter_9

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by email: params[:session][:email].downcase
 
-    if user && user.authenticate(params[:session][:password])
+    if user&.authenticate(params[:session][:password])
       log_in user
+      check_remember user
       redirect_to user
     else
       flash[:danger] = t "invalid_combi"
@@ -14,7 +15,14 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_url
+  end
+
+  private
+
+  def check_remember user
+    params[:session][:remember_me] ==
+      Settings.remember_me_value ? remember(user) : forget(user)
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,6 +28,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit :name, :email, :password,
-                                 :password_confirmation
+      :password_confirmation
   end
 end
